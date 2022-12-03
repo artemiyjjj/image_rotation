e103be1062cc2beff47f95d06a7abf42c7ffcb60
artemioJjJ <artemiyromanoff@gmail.com>
2022-12-03 12:29:00 +0300

    Updated CMake files

---
+++ cmake-build-debug/tester/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/artem/Documents/C_assembly/lab3/tester
+# Build directory: C:/Users/artem/Documents/C_assembly/lab3/cmake-build-debug/tester
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test-1 "D:/UselessProgramms/CLion 2022.2.4/bin/cmake/win/bin/cmake.exe" "-DTEST_DIR=C:/Users/artem/Documents/C_assembly/lab3/tester/tests/1" "-DIMAGE_TRANSFORMER=C:/Users/artem/Documents/C_assembly/lab3/cmake-build-debug/solution/image-transformer.exe" "-DIMAGE_MATCHER=C:/Users/artem/Documents/C_assembly/lab3/cmake-build-debug/tester/image-matcher.exe" "-P" "C:/Users/artem/Documents/C_assembly/lab3/tester/tester.cmake")
+set_tests_properties(test-1 PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/artem/Documents/C_assembly/lab3/tester/CMakeLists.txt;16;add_test;C:/Users/artem/Documents/C_assembly/lab3/tester/CMakeLists.txt;0;")
+add_test(test-2 "D:/UselessProgramms/CLion 2022.2.4/bin/cmake/win/bin/cmake.exe" "-DTEST_DIR=C:/Users/artem/Documents/C_assembly/lab3/tester/tests/2" "-DIMAGE_TRANSFORMER=C:/Users/artem/Documents/C_assembly/lab3/cmake-build-debug/solution/image-transformer.exe" "-DIMAGE_MATCHER=C:/Users/artem/Documents/C_assembly/lab3/cmake-build-debug/tester/image-matcher.exe" "-P" "C:/Users/artem/Documents/C_assembly/lab3/tester/tester.cmake")
+set_tests_properties(test-2 PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/artem/Documents/C_assembly/lab3/tester/CMakeLists.txt;16;add_test;C:/Users/artem/Documents/C_assembly/lab3/tester/CMakeLists.txt;0;")
+add_test(test-3 "D:/UselessProgramms/CLion 2022.2.4/bin/cmake/win/bin/cmake.exe" "-DTEST_DIR=C:/Users/artem/Documents/C_assembly/lab3/tester/tests/3" "-DIMAGE_TRANSFORMER=C:/Users/artem/Documents/C_assembly/lab3/cmake-build-debug/solution/image-transformer.exe" "-DIMAGE_MATCHER=C:/Users/artem/Documents/C_assembly/lab3/cmake-build-debug/tester/image-matcher.exe" "-P" "C:/Users/artem/Documents/C_assembly/lab3/tester/tester.cmake")
+set_tests_properties(test-3 PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/artem/Documents/C_assembly/lab3/tester/CMakeLists.txt;16;add_test;C:/Users/artem/Documents/C_assembly/lab3/tester/CMakeLists.txt;0;")
