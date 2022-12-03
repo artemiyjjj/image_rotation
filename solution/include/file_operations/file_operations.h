33c211ecc72c18eee1238ab995c8830ae524d5e0
artemioJjJ <artemiyromanoff@gmail.com>
2022-12-03 12:33:03 +0300

    Added .c and .h files for opening file

--- solution/include/file_operations/file_operations.h
+++ solution/include/file_operations/file_operations.h
@@ -3,6 +3,6 @@
 #include <stdio.h>
 #include "statuse_codes/status_enums.h"
 
-enum open_stat open_file(FILE* file, const char* filename, const char* mode);
-enum close_stat close_file(FILE* file);
+enum open_stat open_file(FILE** file, const char* filename, const char* mode);
+enum close_stat close_file(FILE** file);
 #endif //IMAGE_TRANSFORMER_FILE_OPERATIONS_H
--- solution/src/file_operations/file_operations.c
+++ solution/src/file_operations/file_operations.c
@@ -1,13 +1,13 @@
 #include "file_operations/file_operations.h"
 
-enum open_stat open_file(FILE* file, const char* filename, const char* mode) {
-    file = fopen(filename, mode);
-    if (!file) { return OPEN_ERR; }
+enum open_stat open_file(FILE** file, const char* filename, const char* mode) {
+    *file = fopen(filename, mode);
+    if (!*file) { return OPEN_ERR; }
     return OPEN_OK;
 }
 
-enum close_stat close_file(FILE* file) {
+enum close_stat close_file(FILE** file) {
     if (file == NULL) { return CLOSE_ERR_NO_STREAM; }
-    else if (fclose(file)) { return CLOSE_FAILED; }
+    else if (fclose(*file)) { return CLOSE_FAILED; }
     else return CLOSE_OK;
 }
